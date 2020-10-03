9a2214532221815e24f232c0a5973a2cdf0967e5
fernando.ishikawa <fernando.ishikawa@skipthedishes.ca>
2020-10-03 18:09:47 -0500

    Adding icons to refill steps

--- src/screens/filling/components/Instructions.tsx
+++ src/screens/filling/components/Instructions.tsx
@@ -1,29 +1,55 @@
 import React, { FC } from 'react';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 import styled from 'styled-components';
+import {
+  SvgIconComponent,
+  Input,
+  Update,
+  ArrowDownward,
+  TapAndPlay,
+} from '@material-ui/icons';
 
 export const BUTTON_LABEL = 'Recarregar';
 
-const FILLING_INSTRUCTIONS = [
-  'Coloque seu frasco na abertura ao lado',
-  'Feche a tampa da abertura',
-  `Pressione o botao "${BUTTON_LABEL}"'`,
-  'Aguarde seu novo perfume',
+interface IInstructions {
+  Icon: SvgIconComponent;
+  label: string;
+}
+
+const FILLING_INSTRUCTIONS: IInstructions[] = [
+  {
+    label: 'Coloque seu frasco na abertura ao lado',
+    Icon: Input,
+  },
+  {
+    label: 'Feche a tampa da abertura',
+    Icon: ArrowDownward,
+  },
+  {
+    label: `Pressione o botao "${BUTTON_LABEL}"'`,
+    Icon: TapAndPlay,
+  },
+  {
+    label: 'Aguarde seu novo perfume',
+    Icon: Update,
+  },
 ];
 
 const Text = styled(ListItemText)`
   .MuiTypography-body1 {
     font-family: 'Open Sans';
+    margin-left: 15px;
   }
 `;
 
 const Instructions: FC = () => {
   return (
     <List>
-      {FILLING_INSTRUCTIONS.map((instruction) => {
+      {FILLING_INSTRUCTIONS.map(({ label, Icon }) => {
         return (
-          <ListItem key={instruction}>
-            <Text primary={instruction} />
+          <ListItem key={label}>
+            <Icon />
+            <Text primary={label} />
           </ListItem>
         );
       })}
