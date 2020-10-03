aa39188b12502b2fcc15352eda5375884a279135
fernando.ishikawa <fernando.ishikawa@skipthedishes.ca>
2020-10-02 21:41:25 -0500

    Configs api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,29 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { apiUrl, apiKey } from '../config';
 
 const config: AxiosRequestConfig = {
   baseURL: apiUrl,
   timeout: 10000,
   headers: {
+    apiKey,
     'Content-Type': 'application/json',
-    apiKey: apiKey,
   },
 };
 
 const api = axios.create(config);
+
+const responseInterceptor = (response: AxiosResponse) => {
+  return response.data;
+};
+
+const errorInterceptor = (error: any) => {
+  const {
+    response: { statusText },
+    message,
+  } = error;
+  throw new Error(statusText || message);
+};
+
+api.interceptors.response.use(responseInterceptor, errorInterceptor);
 
 export default api;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export const apiUrl = 'https://portal.stg.eugenio.io/api/v1';
 export const apiKey = process.env.API_KEY;
+export const tenant = process.env.TENANT;
